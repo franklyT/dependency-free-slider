16f64958ef5029d69b7f1b0f4e4923dad1ede9b4
maldici <48483484+maldici@users.noreply.github.com>
2019-07-07 23:42:08 -0400

    elements

--- src/main.js
+++ src/main.js
@@ -166,11 +166,12 @@ carousel.controls.rightControl.addEventListener('click', () => {
 
 /* Five star markup */
 document.addEventListener('DOMContentLoaded', () => {
-  for (const i of Array(carousel.options.starCount)) {
-    Array.from(document.getElementById('Testimonials').getElementsByTagName('ul')).map((x) => {
-      x.innerHTML
-        /* This SVG is available for use under the SIL Open Font License 1.1, Creative Commons Attribution 4.0, and MIT License. Please refer to https://fontawesome.com/license for more information. */
-        += "<li class=list-inline-item></li><i><svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='star' class='svg-inline--fa fa-star fa-w-18' role='img' xmlns='http://www.w3.org/2000/svg' width='18px' height='18px' viewBox='0 0 576 512'><path stroke='black' fill='currentColor' stroke-opacity='0.8' stroke-width='75px' d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'> </path></svg></i>";
-    });
-  }
+  document.querySelectorAll('.star-rating > ul').forEach((elm) => {
+    for (let i = 0; i < carousel.options.starCount; i += 1) {
+      const starAppender = document.createElement('i');
+      /* This SVG is available for use under the SIL Open Font License 1.1, Creative Commons Attribution 4.0, and MIT License. Please refer to https://fontawesome.com/license for more information. */
+      starAppender.innerHTML = "<svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='star' class='svg-inline--fa fa-star fa-w-18' role='img' xmlns='http://www.w3.org/2000/svg' width='18px' height='18px' viewBox='0 0 576 512'><path stroke='black' fill='currentColor' stroke-opacity='0.8' stroke-width='75px' d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'> </path></svg>";
+      elm.appendChild(starAppender);
+    }
+  });
 });
